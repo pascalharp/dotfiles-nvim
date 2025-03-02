82042c2673ce8efbbdbcbaccb693b570b6631057
Pascal Harprecht <p.harprecht@gmail.com>
2025-03-02 19:10:05 +0100

    plugins:which-key: add group descriptions

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,17 +2,10 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
+    preset = "modern",
+    spec = {
+      { "<leader>f", desc = "[F]ind" },
+      { "<leader>l", desc = "[L]SP" },
+    }
   },
 }
